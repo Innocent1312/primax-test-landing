baa0ed162afa2a1c39668dbc5653ea32b94ac987
Innocent1312 <anthonydev33@gmail.com>
2020-01-14 13:39:38 +0200

    video in modal window added

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import './Video.scss'
+import {Button, Modal} from "semantic-ui-react";
 
 const Video = () => (
   <div className="video-container" id="video">
-    <iframe
-      title="YouTube Video"
-      className="video"
-      width="800"
-      height="500"
-      src="https://www.youtube.com/embed/a3ICNMQW7Ok"
-      frameBorder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen />
+    <Modal trigger={<Button color="red">Play Video</Button>}>
+      <Modal.Header>Video</Modal.Header>
+      <Modal.Content video>
+        <iframe
+          title="YouTube Video"
+          className="video"
+          width="800"
+          height="500"
+          src="https://www.youtube.com/embed/a3ICNMQW7Ok"
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen />
+      </Modal.Content>
+    </Modal>
+
   </div>
 );
 
